f8380628ad2a49a165b067fa3bebff533c14b410
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-04-29 19:52:13 +0300

    Add iter_tokes, Token, tokens_dict

--- iter_tokens.py
+++ iter_tokens.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Iterator
+from dataclasses import dataclass
+import re
 
 
 tokens_dict = {
-    "NUMBER": r"[\d]+",
+    "ID": r"[a-zA-Z]+\d*",
+    "NUMBER": r"\d+",
     "LPAREN": r"\(",
     "RPAREN": r"\)",
+    "EQ": r"=",
+    "PLUS": r"\+",
+    "MINUS": r"-",
+    "STAR": r"\*",
+    "DIVIDE": r"/",
+    "WS": r"\s+",
 }
+
+test_str = "2+(3+4)*5"
+
+
+@dataclass
+class Token:
+    name: str
+    value: str
+
+
+def iter_tokens(s: str) -> Iterator[Token]:
+    master_pat = "|".join(
+        rf"(?P<{tok_key}>{tok_re})" for tok_key, tok_re in tokens_dict.items()
+    )
+    for match in re.finditer(master_pat, s):
+        yield Token(match.lastgroup, match.group())
+
+
+if __name__ == "__main__":
+    for tok in iter_tokens(test_str):
+        print(tok)
